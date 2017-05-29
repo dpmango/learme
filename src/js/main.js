edd041530e0eb9c1ef1cbc4b4172b3165a40b63b
dpmango <xs290@me.com>
2017-05-29 14:33:22 +0300

    validation rules

--- src/js/main.js
+++ src/js/main.js
@@ -9,12 +9,33 @@ $(function(){
   var lessons = $('ol.js-sortable').sortable({
     group: 'lessons',
     containerPath: '> div.collapse',
+    isValidTarget: function ($item, container) {
+      // check section moves
+      if ( $item.is('.spoiler_box') ){
+        // if section is moved into global list
+        if ( container.el.is('.js-sortable') ){
+          return true
+        } else {
+          return false
+        }
+      // check item moves
+      } else if ( $item.is('.sp_line') ) {
+        if ( container.el.is('.sp_content') ){
+          return true
+        } else {
+          return false
+        }
+      // dissallow everything else
+      } else {
+        return false
+      }
+    },
     onDrop: function ($item, container, _super) {
       data = lessons.sortable("serialize").get();
       jsonString = JSON.stringify(data, null, ' ');
       // data returns array and jsonString is more ajax friednly
-      // console.log(data);
-      console.log(jsonString);
+      console.log(data);
+      // console.log(jsonString);
 
       // call ajax calling function
       // updateList();
