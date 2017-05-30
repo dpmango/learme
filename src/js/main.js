d2ed1a457350dd214f05d7097e33155a3ca2354e
dpmango <xs290@me.com>
2017-05-30 19:23:01 +0300

    hold item before absoluting, corrected data output

--- src/js/main.js
+++ src/js/main.js
@@ -33,12 +33,35 @@ $(function(){
       //   return false
       // }
     },
+    onDrag: function ($item, position, _super, event) {
+      // draggin itself
+      $item.css(position)
+    },
+    onDragStart: function ($item, container, _super, event) {
+      // console.clear();
+      // copy element
+      var createdEl = $item.addClass('moving').wrap('<span/>').parent().html();
+      container.el.append( createdEl );
+      $item.removeClass('moving'); // delete class as this element is absoluted
+
+      // super execution
+      $item.css({
+        height: $item.outerHeight(),
+        width: $item.outerWidth()
+      })
+      $item.addClass(container.group.options.draggedClass)
+      $("body").addClass(container.group.options.bodyClass)
+    },
     onDrop: function ($item, container, _super) {
+      // erase copied element
+      $('.moving').remove();
+
+      // Get the data tree
       data = lessons.sortable("serialize").get();
       jsonString = JSON.stringify(data, null, ' ');
       // data returns array and jsonString is more ajax friednly
-      console.log(data);
-      // console.log(jsonString);
+      // console.log(data);
+      console.log(jsonString);
 
       // call ajax calling function
       // updateList();
